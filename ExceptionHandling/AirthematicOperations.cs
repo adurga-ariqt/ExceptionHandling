1fa113cd68df9818f3a34965f47f0566657dcbb1
adurga-ariqt <adurga@ariqt.com>
2022-11-14 20:32:17 +0530

    Add project files.

---
+++ ExceptionHandling/AirthematicOperations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public  class AirthematicOperations
+    {
+
+        public void dividing()
+        {
+            try
+            {
+                int a, b, c;
+                Console.WriteLine("Enter A value:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter b value:");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("The result is:" + c);
+                Console.WriteLine();
+            }
+
+
+
+            catch (DivideByZeroException)
+
+            {
+                Console.WriteLine("b must be non zero");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("b must be a number");
+            }
+            finally
+            {
+                Console.WriteLine("this is final block");
+            }
+
+            try
+            {
+
+
+                int d, e, f;
+                Console.WriteLine("Enter d value:");
+                d = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter b value:");
+                e = Convert.ToInt32(Console.ReadLine());
+                f = d / e;
+                Console.WriteLine("The result is:" + f);
+                Console.ReadLine();
+            }
+
+
+
+            catch (FormatException)
+            {
+                Console.WriteLine("b must be a number");
+            }
+            finally
+            {
+                Console.WriteLine("this is final block");
+            }
+        }
+    }
+
+
+    }
+
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExceptionHandling;
+
+Console.WriteLine("Hello, World!");
+AirthematicOperations obj = new AirthematicOperations();
+obj.dividing();
